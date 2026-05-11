21bfa02614785f10a207b696c4a607d75df04224
Uzeman76 <justinberthelot76160@gmail.com>
2026-05-11 15:41:06 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './components/Login/Login.css';
+import Login from './components/login/Login';
+import React, { useState } from 'react';
+
+
+function App() {
+  return ( <Login /> );
+}
+export default App;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://89.80.248.46:84/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
+      
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.log("Erreur réseau");
+    }
+  };
+
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        {/* Ouverture du formulaire */}
+        <form onSubmit={handleSubmit}>
+          {/* Champ email */}
+          <label class="formu">
+              email :
+            <input type="email" name="email" placeholder="test@gmail.com" required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+
+          <br />
+          {/* Champ mot de passe */}
+          <label  class="formu">
+            mot de passe :
+            <input type="password" name="password" placeholder="01234567" required value={password} // Liaison ajoutée
+              onChange={(e) => setPassword(e.target.value)}/> 
+          </label>
+
+          <br />
+          {/* Bouton d'envoi du formulaire */}
+          <input type="submit" value="Envoyer" class="bouton"/>
+        </form>
+      </header>
+    </div>  );
+};
+
+export default Login;
