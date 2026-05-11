d3ec20c2db2d2e4e3419fda5516e4304965cbaa6
Uzeman76 <justinberthelot76160@gmail.com>
2026-05-11 22:07:54 +0200

    Subscribe page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './components/Login/Login.css';
 import Login from './components/login/Login';
+import Subscribe from './components/Subscribe/Subscribe';
 import React, { useState } from 'react';
-
 
 function App() {
   return ( <Login /> );
+          
 }
+
 export default App;
