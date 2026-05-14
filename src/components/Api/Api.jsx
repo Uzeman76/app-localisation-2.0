4536dbf835c1802c79f98b2eaf5dbeef8287f9fd
louis <louisjean.berthelot76800@gmail.com>
2026-05-14 22:55:31 +0200

    feat : découpage en service et controller pour le login utilisateur
    ici j'ai découpé, j'ai utilisé la fonction de login pour te donner un exemple de ce que tu pourrait utiliser et continuer d'optimiser

--- src/components/Api/Api.jsx
+++
@@ -1,7 +0,0 @@
-const BASE_URL = 'http://89.80.248.46:84/'
-
-export const getApiUrl = (endpoint) => {
-
-    const cleanEndpoint = endpoint.startWith('/') ? endpoint : `/${endpoint}`;
-    return `${BASE_URL}${cleanEndpoint}`;
-};
